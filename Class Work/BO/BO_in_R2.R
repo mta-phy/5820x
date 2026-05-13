40f21fd56d271404274edc14d163f8a41cb3db5c
Mohammad Tanvir Ahmed <tanvir.phy43@gmail.com>
2026-05-12 21:02:16 -0500

    update

---
+++ Class Work/BO/BO_in_R2.R
@@ -0,0 +1,361 @@
+##################################CHANGE01###################################
+# IF YOU HAVE NOT INSTALLED FOLLOWING LIBRARIES BEFORE, UNCOMMENT NEXT LINES (REMOVE #) AND INSTALL THEM.
+# install.packages(c(
+#   "mlr3mbo",
+#   "mlr3",
+#  "mlr3learners",
+#  "bbotk",
+#  "data.table",
+#   "tibble",
+#   "R.utils",
+#   "ranger",
+#   "DiceKriging"
+# ))
+# AFTER INSTALLING, COMMENT THE LINES.
+# %%
+library(mlr3mbo)
+library(mlr3)
+library(mlr3learners)
+library(bbotk)
+library(data.table)
+library(tibble)
+library(R.utils)
+library(ranger)
+library(DiceKriging)
+# %%
+##################################CHANGE02###################################
+#REPLACE WITH THE FILE PATH FOR THE INITIAL DATA ON YOUR COMPUTER
+file <- "D:/MSE/5820x/Class Work/BO/ML_initial.csv"
+data <- as.data.table(read.csv(file))
+data
+column_names <- names(data)
+print(column_names)
+#%%
+# Determine input, output, and direction columns
+output_columns <- tail(column_names, 1)
+input_columns <- setdiff(column_names, output_columns)
+print(paste("Input columns: ", toString(input_columns)))
+print(paste("Output columns: ", toString(output_columns)))
+# if single output, use e.g. c("minimize"), length must match output_columns!
+directions <- c("minimize")
+# Concatenate input and output columns
+selected_columns <- c(input_columns, output_columns)
+# Subset the data
+dt <- data[, ..selected_columns]
+dt
+#%%
+round_to_nearest <- function(x, metadata) {
+  to_nearest = metadata$to_nearest
+  parameter_info = metadata$parameter_info
+  print("Metadata$to_nearest:")
+  print(to_nearest)
+  print("Metadata$X_columns:")
+  print(parameter_info)
+  if (is.data.table(x) || is.data.frame(x)) {
+    print("Column names in x:")
+    print(names(x))
+    print("Parameter names in metadata$parameter_info:")
+    print(names(metadata$parameter_info))
+    for (col_name in names(x)) {
+      print(col_name)
+      if (col_name %in% names(metadata$parameter_info)) {
+        print("First if in the loop")
+        print(paste("Column found in metadata$parameter_info:", col_name))
+        col = x[[col_name]]
+        if (is.numeric(col)) {
+          nearest = as.numeric(to_nearest[[col_name]])
+          print(paste("Column:", col_name))
+          print(paste("Nearest value:", nearest))
+          print("Values before rounding:")
+          print(col)
+          x[[col_name]] = round(col / nearest) * nearest
+          print("Values after rounding:")
+          print(x[[col_name]])
+        }
+      } else {
+        print(paste("Skipping column (not in metadata$parameter_info):", col_name))
+      }
+    }
+  } else if (is.numeric(x)) {
+    nearest = as.numeric(to_nearest)
+    print("Nearest value:")
+    print(nearest)
+    print("Values before rounding:")
+    print(x)
+    x <- round(x / nearest) * nearest
+    print("Values after rounding:")
+    print(x)
+  } else {
+    stop("x must be a data.table, data.frame, or numeric vector")
+  }
+  return(x)
+}
+# %%
+save_archive <- function(archive, acq_function, acq_optimizer, metadata) {
+  # Get the current timestamp
+  timestamp = format(Sys.time(), "%Y%m%d%H%M%S")
+  ##################################CHANGE03###################################
+  #REPLACE WITH THE DIRECTORY PATH WHERE YOU WANT TO SAVE THE DATA ON YOUR COMPUTER
+  dir_path = paste0("D:/MSE/5820x/Class Work/BO/", metadata$bucket_name, "/", metadata$batch_number)    
+  # Create the directory if it doesn't exist
+  if (!dir.exists(dir_path)) {
+    dir.create(dir_path, recursive = TRUE)
+  }
+  # Save the objects to files
+  saveRDS(archive, paste0(dir_path,  "/archive.rds"))
+  saveRDS(acq_function, paste0(dir_path, "/acqf.rds"))
+  saveRDS(acq_optimizer, paste0(dir_path, "/acqopt.rds"))
+  # Print the directory path
+  print(paste("RDS files saved in directory:", dir_path))
+}
+update_and_optimize <- function(acq_function, acq_optimizer, tmp_archive, candidate_new, lie, metadata) {
+  acq_function$surrogate$update()
+  acq_function$update()
+  tmp_archive$add_evals(xdt = candidate_new, xss_trafoed = transform_xdt_to_xss(candidate_new, tmp_archive$search_space), ydt = lie)
+  candidate_new = acq_optimizer$optimize()
+  # Round the candidates to the nearest 0.2
+  candidate_new = round_to_nearest(candidate_new, metadata)
+  return(candidate_new)
+}
+add_evals_to_archive <- function(archive, acq_function, acq_optimizer, data, q, metadata) {
+  if (!is.data.table(archive$data)) {
+    stop("archive$data must be a data.table")
+  }
+  print("Metadata$to_nearest:")
+  print(metadata$to_nearest)
+  print("before lie")
+  lie <- data.table()
+  print("after lie")
+  liar <- min
+  print("after liar")
+  acq_function$surrogate$update()
+  print("after acq_function")
+  acq_function$update()
+  candidate <- acq_optimizer$optimize()
+  print("Candidate after optimize before rounding:")
+  print(candidate)
+  # Round the candidates to the nearest 0.2
+  candidate <- round_to_nearest(candidate, metadata)
+  print("Candidate after rounding:")
+  print(candidate)
+  # print(candidate)
+  tmp_archive = archive$clone(deep = TRUE)
+  acq_function$surrogate$archive = tmp_archive
+  # Apply the liar function to each column in archive$cols_y
+  for (col_name in archive$cols_y) {
+    lie[, (col_name) := liar(archive$data[[col_name]])]
+  }
+  # Check if lie is a data.table
+  if (!is.data.table(lie)) {
+    stop("lie is not a data.table")
+  }
+  candidate_new = candidate
+  print("Candidate_new before update loop:")
+  print(candidate_new)
+  for (i in seq_len(q)[-1L]) {
+    candidate_new <- update_and_optimize(acq_function, acq_optimizer,
+                                         tmp_archive, candidate_new,
+                                         lie, metadata)
+    candidate <- rbind(candidate, candidate_new)
+  }
+  candidate_new <- update_and_optimize(acq_function, acq_optimizer,
+                                       tmp_archive, candidate_new,
+                                       lie, metadata)
+  # Iterate over each column in candidate
+  for (col in names(candidate_new)) {
+    # If the column is numeric, round and format it
+    if (is.double(candidate_new[[col]])) {
+      candidate_new[[col]] <- format(round(candidate_new[[col]], 2), nsmall = 2)
+    }
+  }
+  save_archive(archive, acq_function, acq_optimizer, metadata)
+  return(list(candidate, archive, acq_function))
+}
+load_file <- function(files, pattern) {
+  # Check if there are files that match the pattern
+  matched_files = grep(pattern, files, value = TRUE)
+  if (length(matched_files) == 0) {
+    stop(paste("No", pattern, "files found"))
+  }
+  # Filter for the latest file
+  latest_file = max(matched_files)
+  # Check if latest_file is a valid file
+  if (!file.exists(latest_file)) {
+    stop(paste("File does not exist:", latest_file))
+  }
+  # Load the file
+  loaded_file = readRDS(latest_file)
+  return(loaded_file)
+}
+# load_archive must be modified to load the latest metadata-*.json 
+load_archive <- function(metadata) {
+  # Subtract 1 from batch_number to load from the previous batch
+  previous_batch_number = as.integer(metadata$batch_number) - 1
+  ##################################CHANGE04###################################
+  #REPLACE WITH THE DIRECTORY PATH WHERE YOU WANT TO SAVE THE DATA ON YOUR COMPUTER
+  files = list.files(path = paste0("D:/MSE/5820x/Class Work/BO/", metadata$bucket_name, "/",
+                                   previous_batch_number), pattern = "*.rds", full.names = TRUE)
+  # Load the acqf-, acqopt-, and archive- files
+  acqf = load_file(files, "acqf")
+  acqopt = load_file(files, "acqopt")
+  archive = load_file(files, "archive")
+  acqf$surrogate$archive = archive
+  return(list(archive, acqf, acqopt))
+}
+#%%
+propose_experiment <- function(data, metadata) {
+  set.seed(metadata$seed)
+  data <- as.data.table(data) # data.csv is queried `table`
+  print("Column names in data table:")
+  print(colnames(data))
+  search_space_list <- list()
+  # Loop through metadata$parameter_info
+  for (param_name in names(metadata$parameter_info)) {
+    print(param_name)
+    param_info <- metadata$parameter_info[[param_name]]
+    param_range <- metadata$parameter_ranges[[param_name]]
+    print(paste("Param info:", param_info))
+    print(paste("Param range:", param_range))
+    # Check if param_info is 'object', if so, no need to convert to numeric
+    if (param_info == "object") {
+      search_space_list[[param_name]] <- p_fct(levels = param_range)
+      next
+    }
+    # Convert the results to appropriate type
+    if (param_info == "integer") {
+      lower <- as.integer(param_range[1])
+      upper <- as.integer(param_range[2])
+    } else if (param_info == "float") {
+      lower <- as.numeric(param_range[1])
+      upper <- as.numeric(param_range[2])
+    }
+    # Check if lower or upper is NA
+    if (is.na(lower) | is.na(upper)) {
+      print(paste("lower or upper is NA for param_name:", param_name))
+      next
+    }
+    # Add the parameter to the search space
+    if (param_info == "float") {
+      search_space_list[[param_name]] <- p_dbl(lower = lower, upper = upper)
+    } else if (param_info == "integer") {
+      search_space_list[[param_name]] <- p_int(lower = lower, upper = upper)
+    }
+  }
+  search_space <- do.call(ps, search_space_list)
+  codomain_list = list()
+  # Loop through metadata$output_column_names
+  for (output_name in metadata$output_column_names) {
+    print(paste("Adding output to codomain with id: ", output_name))
+    direction <- toString(metadata$directions[[output_name]])
+    # Add the output to the codomain
+    codomain_list[[output_name]] <- p_dbl(tags = direction)
+  }
+  codomain <- do.call(ps, codomain_list)
+  archive <- ArchiveBatch$new(search_space = search_space, codomain = codomain)
+  print("Archive before add_evals:")
+  print(archive)
+  # Use parameter_info in the subset operation
+  archive$add_evals(xdt = data[, names(metadata$parameter_info), with = FALSE],
+                    ydt = data[, metadata$output_column_names, with = FALSE])
+  print("Model archive so far: ")
+  print(archive)
+  # Determine the number of objectives
+  num_objectives <- length(metadata$output_column_names)
+  print("Number of Objectives: ")
+  print(num_objectives)
+  if (num_objectives == 1) {
+    # Determaine the surrogate model: lrn("rgr.km") for Gaussian Process, default_rf() for Random forest
+    surrogate <- srlrn(lrn("regr.km"), archive = archive)
+    #surrogate <- srlrn(default_rf(), archive = archive)
+    acq_function <- acqf("ei", surrogate = surrogate)
+  } else {
+    #surrogate <- srlrn(list(lrn("regr.km"), lrn("regr.km")), archive = archive)
+    surrogate <- srlrn(list(default_rf(), default_rf()), archive = archive)
+    acq_function <- acqf("ehvi", surrogate = surrogate)
+  }
+  print(names(dt))
+  print("metadata$parameter_info")
+  print(names(metadata$parameter_info))
+  print("metadata$output_column_names")
+  print(metadata$output_column_names)
+  # single or multi
+  acq_optimizer <- acqo(opt("random_search", batch_size = 1000),
+                        terminator = trm("evals", n_evals = 1000),
+                        acq_function = acq_function)
+  print("After acq_optimizer")
+  q <- as.integer(metadata$num_random_lines)
+  print("Before result")
+  result <- add_evals_to_archive(archive, acq_function,
+                                 acq_optimizer, data, q, metadata)
+  print("After result")
+  candidate <- result[[1]]
+  archive <- result[[2]]
+  acq_function <- result[[3]]
+  print("RESULT SO FAR:")
+  print(result)
+  x2 <- candidate[, names(metadata$parameter_info), with = FALSE]
+  print("New candidates: ")
+  print(x2)
+  x2_dt <- as.data.table(x2)
+  data <- rbindlist(list(data, x2_dt), fill = TRUE)
+  # Determine the number of columns to exclude
+  num_exclude <- if (length(metadata$output_column_names) > 1) 3 else 2
+  # Ensure num_exclude does not exceed the number of columns in data
+  num_exclude <- min(num_exclude, ncol(data))
+  print("!! Full data after adding new candidates column: ")
+  print(data)
+  ##################################CHANGE05###################################
+  #REPLACE WITH THE DIRECTORY PATH WHERE YOU WANT TO SAVE THE DATA ON YOUR COMPUTER
+  dir_path = paste0("D:/MSE/5820x/Class Work/BO/", metadata$bucket_name, "/", metadata$batch_number)
+  # Save the data table as a CSV file in the same directory
+  data.table::fwrite(data, paste0(dir_path, "/output.csv"))
+  print(paste0("mlr3mbo proposals finished. Results file is saved at: ", dir_path))
+  return(data)
+}
+# %%
+##################################CHANGE06###################################
+#CHANGE THE METADATA ACCORDINGLY 
+metadata <- list(
+  seed = 42,
+  bucket_name = "Test_Function_01",
+  batch_number = "6",
+  parameter_info = list(
+    param1 = "float",  
+    param2 = "float",  
+    param3 = "float"
+    # param4 = "integer",
+    # param5 = "integer",
+    # param6 = "integer",
+    # param7 = "integer" 
+    # Add more parameters as needed
+  ),
+  parameter_ranges = list(
+    param1 = c(-5, 5),  # The range of parameter1
+    param2 = c(-5,5),  
+    param3 = c(-5,5)
+    # param4 = c(-5,5),
+    # param5 = c(-5,5),
+    # param6 = c(-5,5),
+    # param7 = c(-5,5)
+    # Add more ranges as needed
+  ),
+  output_column_names = c("target"),
+  directions = list(
+    target = "minimize"
+  ),
+  num_random_lines = 3,
+  to_nearest = list(
+    param1 = 0.001,  
+    param2 = 0.001,
+    param3 = 0.001
+    # param4 = 0.001,
+    # param5 = 0.001,
+    # param6 = 0.001,
+    # param7 = 0.001
+  )
+)
+#%%
+# Run the experiment function
+result <- propose_experiment(dt, metadata)
+# %%
+
